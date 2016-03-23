109a323e9092c13ab3d166a4fcb663d69bb9f645
tphan40 <tphan40@gatech.edu>
2016-03-22 23:18:24 -0400

    Initial commit from local git

---
+++ BMSiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMSiOS
+//
+//  Created by Thanh Phan on 3/22/16.
+//  Copyright © 2016 Thanh Phan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BMSiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BMSiOS
+//
+//  Created by Thanh Phan on 3/22/16.
+//  Copyright © 2016 Thanh Phan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
